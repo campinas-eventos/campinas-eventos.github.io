0e2dbbe8d9f8a3e23adf1c8ca3923a9a2e17ecfb
pixelorbits <pixelorbits@bits.com>
2026-09-17 11:01:15 -0300

    adiciona link do formulário para coletar sugestões de nomes

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ export default function RootLayout(
                     <Navbar/>
                     <main className="main-content">{children}</main>
                     <footer className="footer">
+                        <p>Quer adicionar uma página ou plataforma ao site? Envie o nome dela por meio deste <a href="https://docs.google.com/forms/d/1MHmd9JxbJCJU85_6hqle0aakbPCOk28_45-Jzjsjjns" target="_blank">formulário</a></p>
                     </footer>
                 </div>
             </body>
